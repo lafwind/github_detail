9ee914a1d8c85ce5ea8eccb491a47dd7e1d70257
Lafwind <lafwind@gmail.com>
2015-02-24 01:21:51 +0800

    完成获得repo list的功能

--- js/app.js
+++ js/app.js
@@ -1,19 +1,62 @@
 var app = angular.module("myApp", []);
 
-app.controller("GitCtrl", function($scope, $http) {
+app.controller("GitCtrl", ["$scope", "$http", "$q", function($scope, $http, $q) {
 
-  $scope.user_info;
 
-  $scope.getuser = function(username) {
+  $scope.user_info = {};
+  $scope.repo_list = {};
+
+  var get_user = function(username) {
     var user_url = "https://api.github.com/users/" + username + '?callback=JSON_CALLBACK';
     console.log(user_url);
 
-    $http.jsonp(user_url).
-      success(function(data) {
-      console.log(data);
-      $scope.user_info = data.data;
-    }).
-      error(function(data) {
+    var defer = $q.defer();
+
+    $http.jsonp(user_url)
+    .success(function(result) {
+      defer.resolve(result.data);
+    })
+    .error(function(err) {
+      defer.reject(err);
     });
+
+    console.log(defer.promise);
+    return defer.promise;
   }
-});
+
+
+  var get_repo = function(repos_url) {
+    var url =  repos_url + '?callback=JSON_CALLBACK';
+
+    var defer = $q.defer();
+
+    $http.jsonp(url).
+      success(function(result) {
+      $scope.repo_list = result.data;
+      defer.resolve(result.data)
+    }).
+      error(function(err) {
+      defer.reject(err);
+    });
+
+    return defer.promise;
+  }
+
+
+  $scope.get_info = function(username) {
+
+    get_user(username)
+    .then(function(result) {
+      // success
+      $scope.user_info = result;
+      var repo_url = result.repos_url;
+
+      get_repo(repo_url)
+      .then(function(result) {
+        $scope.repo_list = result;
+      });
+
+    });
+    console.log($scope.repo_list);
+  }
+}]);
